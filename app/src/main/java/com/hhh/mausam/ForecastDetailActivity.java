17d7a8c83d1e9c8cb1cc145f243f5b4494e58faa
Harsha <harsh.ssd@gmail.com>
2016-12-04 03:32:04 -0800

    Add ShareActionProvider to share forecast details.

--- app/src/main/java/com/hhh/mausam/ForecastDetailActivity.java
+++ app/src/main/java/com/hhh/mausam/ForecastDetailActivity.java
@@ -3,10 +3,14 @@ package com.hhh.mausam;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -49,7 +53,13 @@ public class ForecastDetailActivity extends AppCompatActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+
+        private static final String LOG_TAG = PlaceholderFragment.class.getSimpleName();
+        private static final String MAUSAM_SHARE_HASHTAG = " #Mausam";
+        private String mForecastDetails;
+
         public PlaceholderFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -57,10 +67,39 @@ public class ForecastDetailActivity extends AppCompatActivity {
             Intent intent = getActivity().getIntent();
             View rootView = inflater.inflate(R.layout.fragment_forecast_detail, container, false);
             if(intent != null  && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastDetail = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView)rootView.findViewById(R.id.forecast_detail)).setText(forecastDetail);
+                mForecastDetails = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView)rootView.findViewById(R.id.forecast_detail)).setText(mForecastDetails);
             }
             return rootView;
+        }
+
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.forecast_detail_fragment, menu);
+
+            // Locate MenuItem with ShareActionProvider
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Fetch and store ShareActionProvider
+            ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            // Attach an intent to this ShareActionProvider. Can be updated anytime like
+            // whenever the user selects a new piece of data they would like to share.
+            if(shareActionProvider != null) {
+                shareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "ShareActionProvider is null.");
+            }
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastDetails + MAUSAM_SHARE_HASHTAG);
+            shareIntent.setType("text/plain");
+            return shareIntent;
         }
     }
 }
